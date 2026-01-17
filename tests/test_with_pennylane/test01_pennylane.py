be70bc1f4d86dc8645feae86a93193bdfb023bb7
mamuncseru <mamun.cseian@gmail.com>
2026-01-17 15:21:27 -0500

    testing simple script for pennylane and diagnosis changes

--- tests/test_with_pennylane/test01_pennylane.py
+++ tests/test_with_pennylane/test01_pennylane.py
@@ -1,5 +1,13 @@
 import pennylane as qml
 import numpy as np
+import sys
+import os
+# Ensure we can import hilbertlens from the parent directory
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../")
+)
+sys.path.insert(0, PROJECT_ROOT)
+
 import hilbertlens as hl
 
 def main():
@@ -38,23 +46,25 @@ def main():
 
     # MANUALLY SET THE PARAMETER COUNT
     # This tells the adapter "expect 2 features" so spectrum() knows what to do.
-    lens.adapter.n_params = 2
+    # lens.adapter.n_params = 2
 
     # 4. Run the Full Diagnosis (Spectrum + Geometry)
     # This will automatically run spectrum() and geometry() checks.
     lens.diagnose()
 
     # --- Manual Checks (Optional) ---
+    # Check 1: Geometry Preservation
+    # Automatically generates a Swiss Roll dataset to test topology preservation
+    print("\n--- Running Manual Geometry Check ---")
+    lens.geometry(save_path="pennylane_geometry.png")
     
-    # Check 1: Frequency Spectrum (Capacity)
+        
+    # Check 2: Frequency Spectrum (Capacity)
     # mode='global' sweeps all inputs simultaneously to check total bandwidth
     print("\n--- Running Manual Spectrum Check ---")
     lens.spectrum(mode='global', save_path="pennylane_spectrum.png")
 
-    # Check 2: Geometry Preservation
-    # Automatically generates a Swiss Roll dataset to test topology preservation
-    print("\n--- Running Manual Geometry Check ---")
-    lens.geometry(save_path="pennylane_geometry.png")
+
 
 if __name__ == "__main__":
     main()
